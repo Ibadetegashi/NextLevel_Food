8340c64671f1f9532831bd3a28cb859123f315a0
Ibadetegashi <77028232+Ibadetegashi@users.noreply.github.com>
2024-05-30 10:02:23 +0200

    Populating the root page, organize main-header folder

--- components/main-header.jsx
+++ components/main-header.jsx
@@ -1,26 +1,31 @@
 import Link from "next/link";
+import Image from "next/image";
 
 import logo from "@/public/images/logo.png";
 import classes from "./main-header.module.css";
+import MainHeaderBackground from "./main-header-background";
 
 export default function MainHeader() {
   return (
-    <header className={classes.header}>
-      <Link href="/" className={classes.logo}>
-        <img src={logo.src} alt="Logo" />
-        NextLevel Food
-      </Link>
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link href="/" className={classes.logo}>
+          <Image src={logo} alt="Logo" priority />
+          NextLevel Food
+        </Link>
 
-      <nav className={classes.nav}>
-        <ul>
-          <li>
-            <Link href="/meals">Browes Meals</Link>
-          </li>
-          <li>
-            <Link href="/community">Foodies Community</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Browes Meals</Link>
+            </li>
+            <li>
+              <Link href="/community">Foodies Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
   );
 }
