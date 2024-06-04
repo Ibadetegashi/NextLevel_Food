c809c2434b8612246c92876bccd8cc622450288d
Ibadetegashi <77028232+Ibadetegashi@users.noreply.github.com>
2024-06-04 13:31:51 +0200

    create meal with actions

--- app/meals/share/page.jsx
+++ app/meals/share/page.jsx
@@ -1,5 +1,6 @@
 import ImagePicker from "@/components/meals/image-picker";
 import classes from "./page.module.css";
+import { shareMeal } from "@/lib/actions";
 
 export default function ShareMealPage() {
   return (
@@ -11,7 +12,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form}>
+        <form className={classes.form} action={shareMeal}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -33,13 +34,13 @@ export default function ShareMealPage() {
           <p>
             <label htmlFor="instructions">Instructions</label>
             <textarea
-              id="instructions"
+              id="qinstructions"
               name="instructions"
               rows="10"
               required
             ></textarea>
           </p>
-          <ImagePicker />
+          <ImagePicker name="image" label="Your Image" />
           <p className={classes.actions}>
             <button type="submit">Share Meal</button>
           </p>
