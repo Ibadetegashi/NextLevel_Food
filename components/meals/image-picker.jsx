3e02bb007fee8eb93ae2e09709147e4316c280ec
Ibadetegashi <77028232+Ibadetegashi@users.noreply.github.com>
2024-06-04 11:00:57 +0200

    Preview Image Picked

--- components/meals/image-picker.jsx
+++ components/meals/image-picker.jsx
@@ -1,18 +1,39 @@
 "use client";
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import Image from "next/image";
+
 import classes from "./image-picker.module.css";
 
 export default function ImagePicker({ label, name }) {
+  const [imagePicked, setImagePicked] = useState("");
   const imageRef = useRef();
 
   function handlePickClick() {
     imageRef.current.click();
   }
 
+  function handleImageChange(event) {
+    const file = event.target.files[0];
+    if (!file) {
+      setImagePicked(null);
+      return;
+    }
+
+    const fileReader = new FileReader();
+    fileReader.onload = () => {
+      setImagePicked(fileReader.result);
+    };
+    fileReader.readAsDataURL(file);
+  }
+
   return (
     <div className={classes.picker}>
       <label htmlFor={name}>{label}</label>
       <div className={classes.controls}>
+        <div className={classes.preview}>
+          {!imagePicked && <p>No image picked!</p>}
+          {imagePicked && <Image src={imagePicked} alt="Image Picked" fill />}
+        </div>
         <input
           className={classes.input}
           type="file"
@@ -20,6 +41,7 @@ export default function ImagePicker({ label, name }) {
           name={name}
           accept="image/png, image/jpeg"
           ref={imageRef}
+          onChange={handleImageChange}
         />
       </div>
       <button
