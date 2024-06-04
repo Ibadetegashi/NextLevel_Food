b93d550672f5f232f5427bc1b3e61bf62c630467
Ibadetegashi <77028232+Ibadetegashi@users.noreply.github.com>
2024-06-04 14:17:58 +0200

    Form Validation, added useFormState

--- app/meals/share/page.jsx
+++ app/meals/share/page.jsx
@@ -1,8 +1,13 @@
+"use client";
+import { useFormState } from "react-dom";
+
 import ImagePicker from "@/components/meals/image-picker";
 import classes from "./page.module.css";
 import { shareMeal } from "@/lib/actions";
+import SubmitButton from "@/components/meals/submit-button";
 
 export default function ShareMealPage() {
+  const [state, formAction] = useFormState(shareMeal, { message: null });
   return (
     <>
       <header className={classes.header}>
@@ -12,24 +17,24 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={shareMeal}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
-              <input type="text" id="name" name="name" required />
+              <input type="text" id="name" name="name" />
             </p>
             <p>
               <label htmlFor="email">Your email</label>
-              <input type="email" id="email" name="email" required />
+              <input type="email" id="email" name="email" />
             </p>
           </div>
           <p>
             <label htmlFor="title">Title</label>
-            <input type="text" id="title" name="title" required />
+            <input type="text" id="title" name="title" />
           </p>
           <p>
             <label htmlFor="summary">Short Summary</label>
-            <input type="text" id="summary" name="summary" required />
+            <input type="text" id="summary" name="summary" />
           </p>
           <p>
             <label htmlFor="instructions">Instructions</label>
@@ -37,12 +42,12 @@ export default function ShareMealPage() {
               id="qinstructions"
               name="instructions"
               rows="10"
-              required
             ></textarea>
           </p>
           <ImagePicker name="image" label="Your Image" />
+          {state.message && <p>{state.message}</p>}
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <SubmitButton />
           </p>
         </form>
       </main>
